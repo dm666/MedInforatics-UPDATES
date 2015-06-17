46c43b610303a1a08bb8694a02c6b2e40a60808e
dm666 <medfac666@mail.com>
2015-06-17 16:06:57 +0300

    Добавление иконки тестовой программы. Теперь продолжу работать над редактором вопросов, чтобы было проще.

--- Windows Tests/Windows Tests/MainWindow.Designer.cs
+++ Windows Tests/Windows Tests/MainWindow.Designer.cs
@@ -29,6 +29,7 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainWindow));
             this.button1 = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.listBox1 = new System.Windows.Forms.ListBox();
@@ -112,9 +113,9 @@
             this.Controls.Add(this.label1);
             this.Controls.Add(this.button1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "MainWindow";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "Form1";
             this.ResumeLayout(false);
 
         }
